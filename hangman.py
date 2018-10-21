d939700300ca040520f0d72a4ea20ead80d9756c
Yoshitaka Nakayama <yoshi@YoshitakanoiMac.local>
2018-10-21 09:56:57 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,51 @@
+# Hang man Code
+# Random
+import random
+
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "__________           ",
+              "|                    ",
+              "|        |           ",
+              "|        O           ",
+              "|       /|\          ",
+              "|       / \          ",
+              "|                    "
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "1文字予想してね: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け。正解は{}".format(word))
+
+
+words = ["cat", "dog", "tree", "mountain", "river", "city", "lion"]
+# print(len(words))
+i = random.randint(0, len(words)-1)
+
+hangman(words[i])
